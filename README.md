d2e15cbcd48bd3c540ddd6d5f48988bf2efc888d
Mario Azcunaga <mario.azcunaga@libnova.com>
2025-10-21 19:55:38 +0200

    seeders

--- README.md
+++ README.md
@@ -8,11 +8,12 @@
 
 ### Pasos para el despliegue
 
-1- 
-2- 
-3-
-4- 
-
+1-  git clone <URL>
+2-  cp .env.example .env
+2-  Create Database 'ilustraevento_bd' and set DB_DATABASE into .env
+3-  composer install
+4-  php artisan migrate
+5-  
 
 
 ## Ejemplos de uso
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Hash;
+use Illuminate\Support\Facades\DB;
 class UserSeeder extends Seeder
 {
     /**
@@ -12,10 +13,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-           User::factory()->create([
+        User::truncate();
+        User::factory()->create([
             'name' => 'Usuario admin',
             'email' => 'admin@admin.com',
             'password' => Hash::make("password")
         ]);
+
+        foreach(range(0,99) as $user_n){
+            User::create([
+                "name"=> "user_$user_n",
+                "email"=> "user_$user_n@user.com",
+                "password" => Hash::make("password")
+            ]);
+        }
     }
 }
