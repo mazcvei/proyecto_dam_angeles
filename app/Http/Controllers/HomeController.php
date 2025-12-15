0c17261e1a94513cde558ae2ebff3ec36567f4dc
Ángeles Trejo Croce <153988949+ATreCro@users.noreply.github.com>
2025-12-15 20:42:17 +0100

    Foter, con validaciones y abrir redes sociales en otra pestaña

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Contact;
 use App\Models\User;
 use App\Models\Rol;
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-         return view('home');
-
+        $settings= Setting::first();
+        return view('home',['settings'=>$settings]);
     }
 
     public function storeContact(Request $request)
@@ -26,6 +27,18 @@ class HomeController extends Controller
             'name.string'         => 'El nombre debe ser una cadena de texto.',
             'name.min' => 'El nombre debe ser como mínimo de 10 carácteres.',
             'name.max'       => 'El nombre debe ser como máximo 1000 carácteres.',
+
+            'email.required'   => 'Debe escribir un email.',
+            'email.email'   => 'Debe ser un email correcto.',
+
+            'phone.required'   => 'Debe escribir un teléfono.',
+            'phone.regex'   => 'el teléfono debe empezar por 6 o 7 y contener 9 dígitos.',
+
+            'message.required'   => 'Debe escribir un mensaje.',
+            'message.string'         => 'El mensaje debe ser una cadena de texto.',
+            'message.min' => 'El mensaje debe ser como mínimo de 10 carácteres.',
+            'message.max'       => 'El mensaje debe ser como máximo 1000 carácteres.',
+
         ]);
 
         Contact::create([
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,8 +6,7 @@
             <div class="col-md-4 col-sm-12">
                 <h5 class="fw-bold">IlustraEvento</h5>
                 <p class="small text-muted">
-                    EDITAR
-                    Tu descripción breve aquí. Servicios, productos o información relevante de tu sitio.
+                {{ $settings->description }}
                 </p>
             </div>
 
@@ -15,8 +14,8 @@
             <div class="col-md-4 col-sm-12">
                 <h5 class="fw-bold">Contacto</h5>
                 <ul class="list-unstyled small">
-                    <li><i class="bi bi-envelope me-2"></i>EDITAR </li>
-                    <li><i class="bi bi-telephone me-2"></i> EDITAR</li>
+                    <li><i class="bi bi-envelope me-2"></i>{{$settings->email_contact}} </li>
+                    <li><i class="bi bi-telephone me-2"></i> {{$settings->phone_contact}}</li>
                 </ul>
             </div>
 
@@ -24,7 +23,7 @@
             <div class="col-md-4 col-sm-12">
                 <h5 class="fw-bold">Síguenos</h5>
                 <div class="d-flex gap-3">
-                    <a href="https://www.instagram.com/ilustrasetting/" class="text-light fs-5"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="{{ $settings->url_instagram }}" class="text-light fs-5" target="_blank"><i class="fa-brands fa-instagram" ></i></a>
                 </div>
             </div>
 
